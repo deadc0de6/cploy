f5b55c6fe81a3ce7d8a732ef05a4b70638c01395
deadc0de6 <info@deadc0de.ch>
2018-03-26 20:11:38 +0200

    commonpath is not in py3.3 and py3.4

--- setup.py
+++ setup.py
@@ -30,8 +30,6 @@ setup(
     license='GPLv3',
     classifiers=[
             'Development Status :: 5 - Production/Stable',
-            'Programming Language :: Python :: 3.3',
-            'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
             'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
